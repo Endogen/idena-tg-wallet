8d386598ea95b9934b8ce3951b1197cbe11ee4a7
Endogen <081081@gmx.net>
2019-12-25 01:09:29 +0100

    Added command 'epoch'

---
+++ idena/plugins/epoch/epoch.py
@@ -0,0 +1,29 @@
+import idena.emoji as emo
+import logging
+
+from telegram import ParseMode
+from idena.plugin import IdenaPlugin
+
+
+class Epoch(IdenaPlugin):
+
+    @IdenaPlugin.threaded
+    @IdenaPlugin.send_typing
+    def execute(self, bot, update, args):
+        epoch = self.api().epoch()
+
+        if "error" in epoch:
+            error = epoch["error"]["message"]
+            msg = f"{emo.ERROR} Couldn't retrieve epoch info: {error}"
+            update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
+            logging.error(msg)
+            return
+
+        epoch = epoch["result"]
+
+        msg = f"`Epoch: {epoch['epoch']}`\n" \
+              f"`Current Period: {epoch['currentPeriod']}`\n\n" \
+              f"`Current Validation:\n{epoch['currentValidationStart']}`\n\n" \
+              f"`Next Validation:\n{epoch['nextValidation']}`" \
+
+        update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
