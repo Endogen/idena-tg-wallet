4805dd5b883bf9dad6f5b0a827bdcb051b91ada1
Endogen <081081@gmx.net>
2019-12-29 00:19:53 +0100

    Added new balance check to 'transactions' command

--- idena/plugins/transactions/transactions.py
+++ idena/plugins/transactions/transactions.py
@@ -13,6 +13,12 @@ class Transactions(IdenaPlugin):
     hash = None
     count = None
 
+    def __enter__(self):
+        if self.config.get("balance_check", "active"):
+            interval = self.config.get("balance_check", "interval")
+            self.repeat_job(self._balance_check, interval)
+        return self
+
     @IdenaPlugin.threaded
     @IdenaPlugin.send_typing
     def execute(self, bot, update, args):
@@ -147,3 +153,58 @@ class Transactions(IdenaPlugin):
             msg,
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True)
+
+    def _balance_check(self, bot, job):
+        address = self.api().address()
+
+        if "error" in address:
+            error = address["error"]["message"]
+            msg = f"{emo.ERROR} Couldn't retrieve address: {error}"
+            logging.error(msg)
+            return
+
+        address = address["result"]
+        transactions = self.api().transactions(address, 50)
+
+        if "error" in transactions:
+            error = transactions["error"]["message"]
+            msg = f"{emo.ERROR} Couldn't retrieve transactions: {error}"
+            logging.error(msg)
+            return
+
+        try:
+            transactions = transactions["result"]["transactions"]
+        except:
+            return
+
+        last = self.config.get("balance_check", "last")
+
+        for transaction in reversed(transactions):
+            if transaction["timestamp"] <= last:
+                continue
+            if transaction["to"] != address:
+                continue
+
+            amount = transaction['amount']
+            amount = f"{float(amount):.2f}"
+            if float(amount) <= 0:
+                continue
+
+            # Remove "0" or "."
+            while amount.endswith("0"):
+                amount = amount[:-1]
+                if amount.endswith("."):
+                    amount = amount[:-1]
+                    break
+
+            # Save transaction as last one
+            self.config.set(transaction["timestamp"], "balance_check", "last")
+
+            # Send "received DNA" message to admins
+            for admin in self.global_config.get("admin", "ids"):
+                try:
+                    msg = f"{emo.BELL} Received `{amount}` DNA"
+                    bot.send_message(admin, msg, parse_mode=ParseMode.MARKDOWN)
+                except Exception as e:
+                    msg = f"Couldn't send 'received DNA' message to ID {str(admin)}: {e}"
+                    logging.warning(msg)
