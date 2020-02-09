9ae5982e2ae4474ad3c0c06fdc56ad987d92e9ba
Endogen <081081@gmx.net>
2020-02-09 01:27:40 +0100

    Changed 'enode' command to 'ipfs'

--- idena/plugins/ipfs/ipfs.py
+++ idena/plugins/ipfs/ipfs.py
@@ -1,3 +1,4 @@
+import idena.utils as utl
 import idena.emoji as emo
 import logging
 
@@ -5,22 +6,22 @@ from telegram import ParseMode
 from idena.plugin import IdenaPlugin
 
 
-class Enode(IdenaPlugin):
+class Ipfs(IdenaPlugin):
 
     @IdenaPlugin.threaded
     @IdenaPlugin.send_typing
     def execute(self, bot, update, args):
-        enode = self.api().enode()
+        ipfs = self.api().ipfs_address()
 
-        if "error" in enode:
-            error = enode["error"]["message"]
-            msg = f"{emo.ERROR} Couldn't retrieve enode: {error}"
+        if "error" in ipfs:
+            error = ipfs["error"]["message"]
+            msg = f"{emo.ERROR} Couldn't retrieve IPFS address: {utl.esc_md(error)}"
             update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
             logging.error(msg)
             return
 
-        msg = f"{emo.NETWORK} Enode value"
+        msg = f"{emo.NETWORK} IPFS address"
         update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
 
-        msg = f"`{enode['result']}`"
+        msg = f"`{ipfs['result']}`"
         update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
