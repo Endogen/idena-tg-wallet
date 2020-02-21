5be0bc9390cbe86044fe99726104f03c55732254
Endogen <081081@gmx.net>
2020-02-21 01:27:57 +0100

    Finalized mining check logic

--- idena/plugins/check/check.py
+++ idena/plugins/check/check.py
@@ -1,10 +1,7 @@
-import os
-import time
 import logging
 import requests
 import idena.emoji as emo
 
-from datetime import datetime
 from telegram import ParseMode
 from idena.plugin import IdenaPlugin
 
@@ -12,6 +9,8 @@ from idena.plugin import IdenaPlugin
 class Check(IdenaPlugin):
 
     def __enter__(self):
+        self.config.set(False, "job_running")
+
         if self.config.get("job_autostart"):
             # Start job to check if node is still mining
             interval = self.config.get("job_interval")
@@ -50,7 +49,8 @@ class Check(IdenaPlugin):
                     return
 
                 # Stop job to check if node is still mining
-                self.get_job(self.get_name()).schedule_removal()
+                job = self.get_job(self.get_name())
+                if job: job.schedule_removal()
 
                 # Save info that node check if deactivated
                 self.config.set(False, "job_running")
@@ -92,19 +92,25 @@ class Check(IdenaPlugin):
 
         result = self.check_online(address['result'])
 
-        if result["success"]:
-            online = result["online"]
-            last_seen = result["last_seen"]
+        if not result["success"]:
+            job.schedule_removal()
+            self.config.set(False, "job_running")
+            msg = f"{emo.CANCEL} Can't watch node"
 
-            if not online:
-                # Send notification to admins
-                for admin in self.global_config.get("admin", "ids"):
-                    try:
-                        msg = f"{emo.ALERT} *Node is not mining!*\nLast seen: {last_seen}"
-                        bot.send_message(admin, msg, parse_mode=ParseMode.MARKDOWN)
-                    except Exception as e:
-                        msg = f"Couldn't send notification that node is not mining to ID {str(admin)}: {e}"
-                        logging.warning(msg)
+        elif not result["online"]:
+            last_seen = result["last_seen"]
+            msg = f"{emo.ALERT} *Node is not mining!*\nLast seen: {last_seen}"
+
+        else:
+            return
+
+        # Send notification to admins
+        for admin in self.global_config.get("admin", "ids"):
+            try:
+                bot.send_message(admin, msg, parse_mode=ParseMode.MARKDOWN)
+            except Exception as e:
+                msg = f"Couldn't send notification that node is not mining to ID {str(admin)}: {e}"
+                logging.warning(msg)
 
     def check_online(self, address):
         api_url = self.config.get("api_url")
@@ -123,12 +129,16 @@ class Check(IdenaPlugin):
         except Exception as e:
             msg = f"{address} Could not reach API: {e}"
             logging.error(msg)
-            return
+
+            result["success"] = False
+            result["message"] = msg
+            return result
 
         # If no last seen date-time, stop to watch node
         if not response or not response["result"] or not response["result"]["lastActivity"]:
             msg = "No 'Last Seen' date. Can not watch node"
             logging.error(f"{address} {msg}")
+
             result["success"] = False
             result["message"] = msg
             return result
@@ -136,27 +146,5 @@ class Check(IdenaPlugin):
         result["success"] = True
         result["online"] = response["result"]["online"]
         result["last_seen"] = response["result"]["lastActivity"]
-
-        # Extract last seen date-time and convert it to seconds
-        last_seen = response["result"]["lastActivity"].split(".")[0]
-        last_seen_date = datetime.strptime(last_seen, "%Y-%m-%dT%H:%M:%S")
-        last_seen_sec = (last_seen_date - datetime(1970, 1, 1)).total_seconds()
-
-        # Load allowed time delta and calculate actual time delta
-        allowed_delta = int(self.config.get("time_delta"))
-        current_delta = int(time.time() - last_seen_sec)
-
-        # Allowed time delta exceeded --> node is offline
-        if current_delta > allowed_delta:
-            if job.context['online']:
-                job.context['online'] = False
-
-        result["success"] = True
-
-        if mining:
-            result["mining"] = mining
-            result["last_seen"] = last_seen
-        else:
-            result["message"] = f"{emo.CANCEL} Mining not possible"
 
         return result
